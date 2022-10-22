ee99daedc0b611105fe460c758d73fc6b03aae8e
vrspaceofficial <vrspaceofficial@gmail.com>
2022-10-22 20:37:59 +0530

    init

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ if(process.env.NODE_ENV === 'production'){
 
 
 app.listen(port, ()=>{
-    console.log(`site was listening at port ${port}`)
+    console.log(`site is listening at port ${port}`)
 })
 
