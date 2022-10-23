79a8fcba33a6dc448b05b127adc09a658a530417
vrspaceofficial <vrspaceofficial@gmail.com>
2022-10-23 21:29:33 +0530

    nothing

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser')
 const port = process.env.PORT||5000;
 const app = express();
+const path  = require('path')
+// const path = require('path')
 
 dotenv.config({path:"./config.env"})
 
@@ -14,9 +16,10 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(require('./routes/auth'))
 
+
+
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static("client/build"))
-    const path  = require('path')
     app.get("*", (req, res)=>{
         res.sendFile(path.resolve(__dirname, 'client', "build", "index.html"))
             })
