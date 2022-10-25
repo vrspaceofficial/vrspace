ead55a950f87ce64ac775383688522e9d89a4590
vrspaceofficial <vrspaceofficial@gmail.com>
2022-10-25 09:11:18 +0530

    app.js home path

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
   const [Data, setData] = useState({name:"", phone:"", email:"", message:""});
   const callAbout = async()=>{
   try {
-    const res = await fetch('https://vr-space-official.herokuapp.com/getData', {
+    const res = await fetch('/getData', {
       method:"GET",
       headers:{
         Accept:"application/json",
@@ -47,7 +47,7 @@ const handleInputs =(e)=>{
 const contactFrom = async(e)=>{
   e.preventDefault();
   const {name, email, phone, message} = Data;
-  const res = await fetch('https://vr-space-official.herokuapp.com/contact',{
+  const res = await fetch('/contact',{
     method:"POST",
     headers:{
       "Content-Type":"application/json"
